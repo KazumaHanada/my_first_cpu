433a3080fcce5812aae98ae0c0a2bb550cba0a30
kazuma hanada <hndkzm1990@gmail.com>
2018-11-06 01:25:35 +0900

    p274 Add decoder

--- cpu_test.v
+++ cpu_test.v
@@ -1,4 +1,4 @@
-//CPUの創り方p222
+//CPUの創り方p272
 //CPUの試用環境を便宜的に作成
 //Analysis & Elaborationのみ行った
 module cpu_test;
@@ -9,13 +9,7 @@ module cpu_test;
 	wire address;
 	wire in;
 	wire out;
-	wire select_a;
-	wire select_b;
-	wire load0;
-	wire load1;
-	wire load2;
-	wire  tmp_out;
 	
-	cpu cpu(clk, n_reset, instr, address, in, out, select_a, select_b, load0, load1, load2, tmp_out);
+	cpu cpu(clk, n_reset, instr, address, in, out);
 
 endmodule
