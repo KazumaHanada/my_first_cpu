3768d87b914bc25699e8a12e4a317ae99f4b2565
kazuma hanada <hndkzm1990@gmail.com>
2018-11-05 22:30:41 +0900

    p215 Add Program Counter

--- cpu_test.v
+++ cpu_test.v
@@ -1,19 +1,20 @@
-//CPUの創り方p211
+//CPUの創り方p215
 //CPUの試用環境を便宜的に作成
 //Analysis & Elaborationのみ行った
 module cpu_test;
 	
 	wire clk;
 	wire n_reset;
+	wire instr;
+	wire address;
 	wire select_a;
 	wire select_b;
 	wire load0;
 	wire load1;
 	wire load2;
 	wire load3;
-	wire im;
 	wire out;
 	
-	cpu cpu(clk, n_reset, select_a, load0, load1, load2, load3, im, out);
+	cpu cpu(clk, n_reset, instr, address, select_a, select_b, load0, load1, load2, load3, out);
 
 endmodule
